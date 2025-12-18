f453082e1722035058415e4bcaca104f0b7d23bd
gustavo <gustavo@gmail.com>
2025-12-17 23:45:24 -0500

    se agrega log

--- src/main/java/com/example/demo/service/PetServiceImpl.java
+++ src/main/java/com/example/demo/service/PetServiceImpl.java
@@ -4,19 +4,26 @@ import com.example.demo.exception.PetAlreadyExistsException;
 import com.example.demo.model.Pet;
 import com.example.demo.repository.PetRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PetServiceImpl implements PetService{
 
+    private static final Logger log = LoggerFactory.getLogger(PetServiceImpl.class);
     private final PetRepository petRepository;
 
     @Override
     public Mono<Pet> savePet(Pet pet) {
+        log.info("name: {}", pet.getName());
         return Mono.fromCallable(() -> {
                     boolean exists =
                             petRepository.existsByNameAndAge(pet.getName(), pet.getAge());
@@ -26,7 +33,7 @@ public class PetServiceImpl implements PetService{
                                 "Pet with same name and age already exists"
                         );
                     }
-
+                    log.info("pet new: {}", pet.getName());
                     return petRepository.save(pet);
                 })
                 .subscribeOn(Schedulers.boundedElastic());
