e5d213d0206ece2c959ee4a7f571d5b41c3051cd
gustavo <gustavo@gmail.com>
2025-12-17 23:15:04 -0500

    se usa lombok en servicio

--- src/main/java/com/example/demo/service/PetServiceImpl.java
+++ src/main/java/com/example/demo/service/PetServiceImpl.java
@@ -2,17 +2,15 @@ package com.example.demo.service;
 
 import com.example.demo.model.Pet;
 import com.example.demo.repository.PetRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
+@RequiredArgsConstructor
 public class PetServiceImpl implements PetService{
 
     private final PetRepository petRepository;
-
-    public PetServiceImpl(PetRepository petRepository) {
-        this.petRepository = petRepository;
-    }
 
     @Override
     public Mono<Pet> savePet(Pet pet) {
